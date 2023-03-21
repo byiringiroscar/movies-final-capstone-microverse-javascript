c85587c3d45eaced87af33e1be9e7629150543ad
byiringiroscar <byiringoroscar@gmail.com>
2023-03-21 23:34:58 +0200

    fetch data from API

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,3 @@
 module.exports = {
-    setupFilesAfterEnv: ['jest-localstorage-mock'],
-  };
+  setupFilesAfterEnv: ['jest-localstorage-mock'],
+};
--- src/index.js
+++ src/index.js
@@ -1,2 +1,40 @@
-import './style.css';
-import './logo.png';
+// import './style.css';
+// import './logo.png';
+
+const movieContainer = document.querySelector('.movie-container');
+class Movies {
+    getMovies = async () => {
+      try {
+        const response = await fetch('https://api.tvmaze.com/shows?page=1');
+        const data = await response.json();
+        return data;
+      } catch (error) {
+        return error;
+      }
+    }
+}
+
+const movie = new Movies();
+
+const update = async () => {
+  const allData = await movie.getMovies();
+  let html = '';
+  for (let i = 0; i < allData.length; i += 1) {
+    const singleData = allData[i];
+    html += `<div class="card">
+        <div class="card-image-container">
+            <img class="card-image" src="${singleData.image.medium}" alt="">
+            <img src="./play.png" alt="" id="play">
+        </div>
+        
+        <div class="card-content">
+            <p class="movie-title">${singleData.name}</p>
+            <img src="./like.png" alt="like" id="like">
+            <p class="like-stat">2 likes</p>
+        </div>
+        <button class="card-button">Comments</button>
+    </div>`;
+  }
+  movieContainer.innerHTML = html;
+};
+update();
