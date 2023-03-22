725acc40061cb2c0866392f5a5b099f5a0249f3a
imranhossainemi <blur-imran@hotmail.com>
2023-03-22 06:32:13 +0600

    Update counters.js

--- src/modules/counters.js
+++ src/modules/counters.js
@@ -1,11 +1,5 @@
-const getMovieCount = (items) => {
-  const result = items.length;
-  return result;
-};
-
 const getCommentCount = (comments) => {
   const res = comments.length;
   return res;
 };
-
-export { getMovieCount, getCommentCount };
+export default getCommentCount;
